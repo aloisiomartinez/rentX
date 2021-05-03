ecccdea8cb1510cde39b3e5fa2146d270358d85d
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-05-03 08:20:50 -0300

    ensureAdmin authenticate middleware

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,13 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import "reflect-metadata";
 
-interface IOptions {
-  host: string;
-}
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  createConnection({
-    ...options,
-  });
-});
+export default async (host = "database"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host,
+    })
+  );
+};
