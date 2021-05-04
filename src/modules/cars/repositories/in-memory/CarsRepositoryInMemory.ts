0ab8930c8d76bad5d9a51891f7fefd56b946dfb4
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-05-04 08:49:40 -0300

    specificationcars migration manytomany

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -43,9 +43,9 @@ class CarsRepositoryInMemory implements ICarsRepository {
     const cars = this.cars.filter((car) => {
       if (
         car.available === true ||
-        ((brand && car.brand === brand) ||
-          (category_id && car.category_id === category_id) ||
-          (name && car.name === name))
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
       ) {
         return car;
       }
