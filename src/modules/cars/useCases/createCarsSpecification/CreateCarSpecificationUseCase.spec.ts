ecfef19b5b3019f6a7e55304f76e4723690c91cf
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-05-05 08:22:45 -0300

    CreateCarSpecification InMemory

--- src/modules/cars/useCases/createCarsSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarsSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,16 +1,21 @@
+import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { SpecificationsRepositoryInMemory } from "@modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory";
 
 import { AppError } from "../../../../shared/errors/AppError";
 import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
 let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
 
 describe("Create Car Specification", () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory();
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
     createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
-      carsRepositoryInMemory
+      carsRepositoryInMemory,
+      specificationsRepositoryInMemory
     );
   });
   it("should not be able to add a new specification to a none-existent car", async () => {
