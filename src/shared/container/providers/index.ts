668c05612c66ae2763b357a157bf59f5640c9c32
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-05-28 15:01:49 -0300

    Changing logic to save a file to save into S3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,10 +4,17 @@ import { IDateProvider } from "@shared/container/providers/DateProvider/IDatePro
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { EtherealMailProvider } from "@shared/container/providers/MailProvider/implementations/EtherealMailProvider";
+import { LocalStorageProvider } from "@shared/container/providers/StorageProvider/implementations/LocalStorageProvider";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayjsDateProvider);
 
 container.registerInstance<IMailProvider>(
   "EtherealMailProvider",
   new EtherealMailProvider()
+);
+
+container.registerSingleton<IStorageProvider>(
+  "StorageProvider",
+  LocalStorageProvider
 );
