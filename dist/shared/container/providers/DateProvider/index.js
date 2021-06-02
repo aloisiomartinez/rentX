ebeee7c243ef52846c0bf4cf9759ea7d594889cf
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-06-02 20:15:07 -0300

    Production Configuration

--- dist/shared/container/providers/DateProvider/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _DayjsDateProvider = require("@shared/container/providers/DateProvider/implementations/DayjsDateProvider");
-
-_tsyringe.container.registerSingleton("DateProvider", _DayjsDateProvider.DayjsDateProvider);
